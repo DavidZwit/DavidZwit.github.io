52dd158a19a5df28b40e2929ebc616201bfb24ca
DavidZwit <davidzwitser@gmail.com>
2016-11-04 09:21:27 +0100

    Adding snap scroll and making the game page look better

--- js/snapScroll.js
+++ js/snapScroll.js
@@ -1,5 +1,21 @@
+snapScroll = function () {
 
 document.addEventListener('wheel', function (e) {
     let scroll = e.wheelDeltaY < 0 ? 1 : -1;
     if (canScrollWindow == true) nextWindow(scroll);
 });
+
+var touchScrollOffsetY = 0;
+var oldTouchScrollOffset = 0;
+document.addEventListener('touchmove', function (e) {
+    touchScrollOffsetY = e.changedTouches[0].clientY;
+
+    let scroll = touchScrollOffsetY - oldTouchScrollOffset;
+    if (canScrollWindow == true) nextWindow(scroll > 0 ? 1 : -1);
+    
+    oldTouchScrollOffset = touchScrollOffsetY;
+});
+
+}
+
+snapScroll();
